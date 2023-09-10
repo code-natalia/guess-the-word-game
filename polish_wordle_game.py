a88e2b6deea52ecaa4e4bff7f34e314744e4aeae
Natalia Gonkiewicz <natalia.gonkiewicz@gmail.com>
2023-09-10 14:48:15 +0200

    Rename file and add polish words

--- polish_wordle_game.py
+++ polish_wordle_game.py
@@ -1,12 +1,12 @@
-import skrypt
+import helpers
 import random
 
-opened_file = open("słowa.txt", "r")
+opened_file = open("polish_words.txt", "r")
 
-numbers_of_letters = skrypt.input_word_len()
+numbers_of_letters = helpers.input_word_len()
 
-words_in_game = skrypt.filter_words(opened_file, numbers_of_letters )
+words_in_game = helpers.filter_words(opened_file, numbers_of_letters )
 
 word = random.choice(words_in_game)
 
-skrypt.game(words_in_game, word, numbers_of_letters)
+helpers.game(words_in_game, word, numbers_of_letters)
